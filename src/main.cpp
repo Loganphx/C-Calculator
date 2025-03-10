42cd263d40d65839c40bfa7a9d06eb201b34779f
Logan Ingram <loganphx1@gmail.com>
2025-03-10 10:14:17 -0700

    Added Key press tracking, and started tracking focus gained and lost.

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,60 @@ int main()
                                    sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
     window.setFramerateLimit(60);
 
+    const auto onClose = [&window](const sf::Event::Closed&)
+    {
+        window.close();
+    };
+    const auto onMousePressed = [&window](const sf::Event::MouseButtonPressed& mousePressed) {
+        if(mousePressed.button == sf::Mouse::Button::Left) {
+            // Left Click!
+        }
+        else if(mousePressed.button == sf::Mouse::Button::Right) {
+            // Right Click!
+        }
+        else if(mousePressed.button == sf::Mouse::Button::Middle) {
+            // Middle Click!
+        }
+    };
+
+    const auto onKeyPressed = [&window](const sf::Event::KeyPressed& keyPressed) {
+        if(keyPressed.scancode == sf::Keyboard::Scancode::W){
+            // W!
+        }
+        else if(keyPressed.scancode == sf::Keyboard::Scancode::A){
+            // A!
+        }
+        else if(keyPressed.scancode == sf::Keyboard::Scancode::S){
+            // S!
+        }
+        else if(keyPressed.scancode == sf::Keyboard::Scancode::D){
+            // D!
+        }
+    };
+
+    const auto onFocusLost = [&window](const sf::Event::FocusLost&) {
+        //myGame.pause();
+        // Background fps is 30.
+        window.setFramerateLimit(30);
+    };
+
+    const auto onFocusGained = [&window](const sf::Event::FocusGained&) {
+        //myGame.resume();
+        // Foreground fps is 60
+        //Todo replace this with a config value
+        window.setFramerateLimit(60);
+    };
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-            else if(const auto* mousePressed = event->getIf<sf::Event::MouseButtonPressed>())
-            {
-                if(mousePressed->button == sf::Mouse::Button::Left) {
-                    // Left Click!
-                }
-                else if(mousePressed->button == sf::Mouse::Button::Right) {
-                    // Right Click!
-                }
-            }
-        }
+        window.handleEvents(onClose, onMousePressed, onKeyPressed, onFocusLost, onFocusGained);
+
+//        while (const std::optional event = window.pollEvent())
+//        {
+////            if (event->is<sf::Event::Closed>())
+////            {
+////                window.close();
+////            }
+//        }
 
         window.clear();
         window.display();
