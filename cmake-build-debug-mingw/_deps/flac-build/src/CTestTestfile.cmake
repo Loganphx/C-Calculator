ee7902a659fd06aeb23eee27230ca2694bef5848
Logan Ingram <loganphx1@gmail.com>
2025-03-10 09:58:13 -0700

    - Updated Cmake version to 3.31

---
+++ cmake-build-debug-mingw/_deps/flac-build/src/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Logan/CLionProjects/C-Calculator/cmake-build-debug-mingw/_deps/flac-src/src
+# Build directory: C:/Users/Logan/CLionProjects/C-Calculator/cmake-build-debug-mingw/_deps/flac-build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("libFLAC")
