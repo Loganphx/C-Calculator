6f3c9a917f41e896e2f69074625a11796c509d2d
Logan Ingram <loganphx1@gmail.com>
2025-03-11 11:15:25 -0700

    Setup rendering of Bounding boxes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <list>
+#include <iostream>
 
-std::list<sf::Transformable> entities = {};
-
+std::list<sf::CircleShape> entities = {};
+sf::Vector2<float> mousePos;
 int main()
 {
     entities = {};
@@ -44,11 +45,19 @@ int main()
         }
     };
 
-//    const auto onMouseMoved = [&window](const sf::Event::MouseMoved& mouseMoved) {
-//        for (int i = 0; i < entities.size(); ++i) {
-//            if(mouseMoved.position.x)
-//        }
-//    };
+    const auto onMouseMoved = [&window](const sf::Event::MouseMoved& mouseMoved) {
+        sf::Vector2<float> pos(mouseMoved.position.x, mouseMoved.position.y);
+        std::cout << "Mouse Pos: " + std::to_string(mouseMoved.position.x) + " " + std::to_string(mouseMoved.position.y) + "\n";
+        mousePos = pos;
+        for (const sf::CircleShape& entity : entities) {
+            sf::FloatRect boundingBox = entity.getGlobalBounds();
+            std::cout << std::to_string(boundingBox.position.x) + " " + std::to_string(boundingBox.position.y) + "\n";
+
+            if(boundingBox.contains(pos)) {
+                std::cout << "Overlap Detected!";
+            }
+        }
+    };
 
     const auto onFocusLost = [&window](const sf::Event::FocusLost&) {
         //myGame.pause();
@@ -66,9 +75,33 @@ int main()
     clock_t current_ticks, delta_ticks;
     clock_t fps = 0;
 
+    sf::Font font("pixel-arial-14.otf"); // Throws sf::Exception if an error occurs
+
+    sf::Text fps_text(font); // a font is required to make a text object
+    fps_text.setCharacterSize(24); // in pixels, not points!
+    fps_text.setFillColor(sf::Color::Red);
+    fps_text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+    sf::CircleShape circle(50.f);
+    circle.setFillColor(sf::Color(100, 250, 50));
+    circle.setPosition({250.f, 250.f});
+    entities.push_back(circle);
+
+    sf::RectangleShape boundBox(circle.getGlobalBounds().size);
+    boundBox.setPosition(circle.getGlobalBounds().position);
+    boundBox.setFillColor(sf::Color::Transparent);
+    boundBox.setOutlineColor(sf::Color::Red);
+    boundBox.setOutlineThickness(2.f);
+
+    sf::Text mouseText(font); // a font is required to make a text object
+    mouseText.setCharacterSize(24); // in pixels, not points!
+    mouseText.setFillColor(sf::Color::Red);
+    mouseText.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    // inside the main loop, between window.clear() and window.display()
+
     while (window.isOpen())
     {
-        window.handleEvents(onClose, onMousePressed, onKeyPressed, onFocusLost, onFocusGained);
+        window.handleEvents(onClose, onMousePressed, onKeyPressed, onFocusLost, onFocusGained, onMouseMoved);
 
 //        while (const std::optional event = window.pollEvent())
 //        {
@@ -79,27 +112,16 @@ int main()
 //        }
 
         current_ticks = clock();
+        fps_text.setString(std::to_string(fps));
+        mouseText.setString(std::to_string(static_cast<int>(mousePos.x)) + " " + std::to_string(static_cast<int>(mousePos.y)));
+        mouseText.setPosition(mousePos);
 
         window.clear();
 
-        sf::Font font("pixel-arial-14.otf"); // Throws sf::Exception if an error occurs
-
-        sf::Text text(font); // a font is required to make a text object
-        text.setString(std::to_string(fps));
-        text.setCharacterSize(24); // in pixels, not points!
-        text.setFillColor(sf::Color::Red);
-        text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-
-
-        // inside the main loop, between window.clear() and window.display()
-        window.draw(text);
-
-        sf::CircleShape shape(50.f);
-        entities.push_back(shape);
-
-        shape.setFillColor(sf::Color(100, 250, 50));
-        shape.setPosition({250.f, 250.f});
-        window.draw(shape);
+        window.draw(fps_text);
+        window.draw(circle);
+        window.draw(boundBox);
+        window.draw(mouseText);
 
         window.display();
 
